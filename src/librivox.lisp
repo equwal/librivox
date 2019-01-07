61ddc40eff416fe620557bc828f4000c58c31dca
Spenser Truex <spensertruexonline@gmail.com>
2019-01-07 15:52:42 -0800

    Code refactoring, splitting to testing fork.

--- src/librivox.lisp
+++ src/librivox.lisp
@@ -1,7 +1,13 @@
 (in-package :librivox)
-
-(defun start (csv-input)
+(defun start ()
   "Just start everything: download, convert, upload."
-  (download)
-  (convert)
-  (upload))
+  (csv-expand (csv-update) :forward t)
+  (convert-all))
+
+(defun csv-update ()
+  "Get a new CSV to keep up with recent audiobook publishing.")
+(defun csv-expand (&key forward)
+  "Convert a CSV of librivox audiobooks into a directory tree filled with files
+to be used in the downloading and conversion portions. `FORWARD' determines if
+the directory tree is being made fresh -> nil or just updated -> t. "
+  )
