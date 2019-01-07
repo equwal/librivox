5691db8015a2eda7dcc18df8778d62c6566b06ce
Spenser Truex <spensertruexonline@gmail.com>
2019-01-07 08:45:14 -0800

    First gihub.

--- src/librivox.lisp
+++ src/librivox.lisp
@@ -1,8 +1,7 @@
 (in-package :librivox)
-;; As far as I can tell counting processor cores is a common way of determining
-;; max threads. FFMPEG does it automatically.
-(defparameter *max-threads* #+multithreading (processor-cores)
-	      #- multithreading 1)
-(defun processor-cores ()
-  #+linux (run-line*-integer-output "grep -c ^processor /proc/cpuinfo~*")
-  #-linux 1)
+
+(defun start (csv-input)
+  "Just start everything: download, convert, upload."
+  (download)
+  (convert)
+  (upload))
---
+++ src/uploader.lisp
@@ -0,0 +1,48 @@
+;;;; Example request:
+;;;; python upload_video.py --file="/tmp/test_video_file.flv"
+;;;; --title="Summer vacation in California"
+;;;; --description="Had fun surfing in Santa Cruz"
+;;;; --keywords="surfing,Santa Cruz"
+;;;; --category="22"
+;;;; --privacyStatus="private"
+
+(in-package #:librivox)
+
+(defun pair-keywords (keywords)
+  "Generate a proper bash call for paired keywords."
+  (labels ((inner (keywords first acc)
+             (if (null keywords)
+                 ""
+                 (if first
+                     (inner (cdr keywords) nil (car keywords))
+                     (inner (cdr keywords) nil (concatenate 'string
+                                                            ","
+                                                            (car keywords)))))))
+    (inner keywords t "")))
+
+(defun upload (&key file
+                 title
+                 description
+                 keywords
+                 category
+                 privacy-status)
+  (run-line (concatenate 'string
+                         "--file=\""
+                         *downloads-dir*
+                         file
+                         "\" "
+                         "--title=\""
+                         title
+                         "\" "
+                         "--description=\""
+                         description
+                         "\" "
+                         "--keywords=\""
+                         (pair-keywords keywords)
+                         "\" "
+                         "--category=\""
+                         category
+                         "\" "
+                         "--privacyStatus=\""
+                         privacy-status
+                         "\"")))
